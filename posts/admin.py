04280945c451b44e667e4341ba33f1134538a421
richie20502 <richie.lugo.recillas.1990@gmail.com>
2023-06-23 08:16:51 -0600

    se agrego en el panel del admin del post el list display

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
-# Register your models here.
+from posts.models import Post
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title','created_at']
