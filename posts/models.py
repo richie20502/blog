58d9713c2f2886a889db7124c127eaff1664ac14
richie20502 <richie.lugo.recillas.1990@gmail.com>
2023-06-23 07:51:25 -0600

    se crearon la smigraciones

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    order = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_created=True)
