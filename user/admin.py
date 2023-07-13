4c4662c15c8b599a87fdb538f3e98c6615dbd7ae
richie20502 <richie.lugo.recillas.1990@gmail.com>
2023-07-12 23:05:24 -0600

    agregar propiedades al modelo usuario y panel administrador

--- user/admin.py
+++ user/admin.py
@@ -4,4 +4,9 @@ from user.models import User
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    pass
+    fieldsets = (
+        (None, {'fields':('username','password')}),
+        ('Información Personal', {'fields':('first_name','last_name','email')}),
+        ('Permisos', { 'fields': ('is_active','is_staff','is_superuser','groups','user_permissions')}),
+        ('Redes Sociale',{'fields':('web_site','twitter',)})
+    )
--- user/models.py
+++ user/models.py
@@ -1,4 +1,6 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    web_site = models.CharField(max_length=255, blank=True)
+    twitter = models.CharField(max_length=255, blank=True)
