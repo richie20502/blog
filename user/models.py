e5735e9a87a46ef525725c69ba755cdd156d2700
richie20502 <richie.lugo.recillas.1990@gmail.com>
2023-07-12 23:09:42 -0600

    se agrego Cambiando el identificador username por email

--- user/models.py
+++ user/models.py
@@ -2,5 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    email = models.EmailField(unique=True)
     web_site = models.CharField(max_length=255, blank=True)
     twitter = models.CharField(max_length=255, blank=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
