2ddf0a74430e35b081d3f78b9183fbb9fd358a85
richie20502 <richie.lugo.recillas.1990@gmail.com>
2023-07-12 21:15:29 -0600

    se agrego drf-yasg

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'posts'
+    'posts',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = './static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
