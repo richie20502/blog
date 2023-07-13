c4cba9089cfb46556a6ee5d6537e3e17c54e6ee8
richie20502 <richie.lugo.recillas.1990@gmail.com>
2023-07-12 22:37:24 -0600

    se agrego en el panel del admin mostrar todo el usuario

--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from user.models import User
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
+    pass
