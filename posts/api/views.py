5420f5e5b2a5016ae0f1bdbde3ef362f85526078
richie20502 <richie.lugo.recillas.1990@gmail.com>
2023-07-12 20:23:05 -0600

    se genero las vistas con el ModelViewSet

--- posts/api/views.py
+++ posts/api/views.py
@@ -1,26 +1,33 @@
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.viewsets import ViewSet
+from rest_framework.viewsets import ViewSet, ModelViewSet
 from rest_framework.response import Response
 from posts.models import Post
 from posts.api.serializer import PostSerializer
 
-class PostViewSet(ViewSet):
-    def list(self, request):
-        # posts = Post.objects.all()
-        #posts = [post.title for post in Post.objects.all()]
-        serializer = PostSerializer(Post.objects.all(), many= True)
-        return Response(status= status.HTTP_200_OK, data=serializer.data)
+class PostModelViewSet(ModelViewSet):
+    serializer_class = PostSerializer
+    queryset =  Post.objects.all()
 
-    def retrieve(self, request, pk: int):
-        post = PostSerializer(Post.objects.get(pk=pk))
-        return Response(status=status.HTTP_200_OK, data=post.data)
 
-    def post(self, request):
-        serializer = PostSerializer(data=request.POST)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(status= status.HTTP_200_OK, data=serializer.data)
+
+#este es con viewset
+# class PostViewSet(ViewSet):
+#     def list(self, request):
+#         # posts = Post.objects.all()
+#         #posts = [post.title for post in Post.objects.all()]
+#         serializer = PostSerializer(Post.objects.all(), many= True)
+#         return Response(status= status.HTTP_200_OK, data=serializer.data)
+#
+#     def retrieve(self, request, pk: int):
+#         post = PostSerializer(Post.objects.get(pk=pk))
+#         return Response(status=status.HTTP_200_OK, data=post.data)
+#
+#     def post(self, request):
+#         serializer = PostSerializer(data=request.POST)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(status= status.HTTP_200_OK, data=serializer.data)
 
 
 # class PostApiView(APIView):
